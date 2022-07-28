9c0e05ab8bfa4897d492eee20404351f02403301
tsunodajapa <gabrieltsunoda@gmail.com>
2022-07-28 12:14:01 -0300

    Initial Commit

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Button from '../Button'
 import { Card } from './styles'
 
-const PokemonCard = ({ name }) => {
+const PokemonCard = ({ name, onClick }) => {
   return (
     <Card>
       <h3>{name}</h3>
+
+      <Button onClick={onClick}>{name}</Button>
     </Card>
   )
 }
--- src/components/PokemonCard/styles.js
+++ src/components/PokemonCard/styles.js
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Card = styled.li`
-    padding: 2rem;
-    background: #fff;
-    border: 1px solid red;
-`
+  padding: 2rem;
+  background: #fff;
+  border: 1px solid red;
+`;
